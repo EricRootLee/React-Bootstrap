1a1baead8f3ccba5f68a7cee058459c106f01afc
Eric Muli <erickiokom@gmail.com>
2020-07-24 10:54:47 +0300

    COlumns

--- src/components/components/alerts.js
+++ src/components/components/alerts.js
@@ -4,17 +4,35 @@ import { Alert } from 'react-bootstrap'
 class AlertsClass extends Component {
     render() {
         return (
-            [
+
+            <div>
+                <Alert variant="success">
+                    <Alert.Heading>Hey NIce heading</Alert.Heading>
+                    <p>Ea ut et sint sunt elit consequat non commodo dolore.</p>
+                    <hr />
+
+                    <p classNameb="mb-0">hello</p>
+
+                </Alert>
+
+
+                {/* [
                 'primary',
                 'danger',
                 'light',
                 'dark'
-            ].map((variant, id) => (
+                ].map((variant, id) => (
                 <Alert key={id} variant={variant}>
-                    This is a {variant} alert of id {id} with { ' '}
+                    This is a {variant} alert of id {id} with {' '}
                     <Alert.Link href="#"> an example  lINK</Alert.Link>
                 </Alert>
-            ))
+            )) */}
+
+
+
+
+
+            </div>
         )
     }
 }
